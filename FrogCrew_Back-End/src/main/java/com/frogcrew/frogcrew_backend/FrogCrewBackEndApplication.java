e60b3fe28a54cb0e91fa80c2ff38e05c412a610f
adessfaith <adessasegura@gmail.com>
2025-04-11 17:54:21 -0500

    Back-End folder added

---
+++ FrogCrew_Back-End/src/main/java/com/frogcrew/frogcrew_backend/FrogCrewBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.frogcrew.frogcrew_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogCrewBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogCrewBackEndApplication.class, args);
+    }
+
+}
---
+++ FrogCrew_Back-End/src/test/java/com/frogcrew/frogcrew_backend/FrogCrewBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frogcrew.frogcrew_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogCrewBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
